502c6d96dd2e11c79a91117d385855dbf72b3bbe
Benjamin Jørgensen <benjamin@bnjamin.com>
2011-05-21 17:45:06 +0200

    Changed the route to a catch all

--- procrastination.rb
+++ procrastination.rb
@@ -9,7 +9,7 @@ motivations = [ "We are what we repeatedly do; excellence, then, is not an act b
                 "You are awesome, especially if you would get some work done."
 ]
 
-get "/" do
+get "/*" do
   motivation = motivations[rand(motivations.size)]
   "<html>" + "<head><title>" + motivation + "</title></head>" + "<body>" + "<div style='width:1000px; padding: 40px; margin: auto; margin-top: 300px; background: #ffff77;'>" + "<h1 style='font-size:40px; text-align: center;'>" + motivation + "</h1>" + "</div>" + "</body>" + "</html>"
 end
